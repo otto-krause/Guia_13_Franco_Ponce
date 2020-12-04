f8e224208a82804ca2aec604706a439440566500
gusgatam <gustavo.gatamora@gmail.com>
2020-12-04 15:57:00 -0300

    Devolución.
    Subir los ejercicios de cada guia de manera individual para su correcta verificación.

--- guia_13_franco_ponce.cpp
+++ guia_13_franco_ponce.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Subir ejercicios de manera individual.
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
